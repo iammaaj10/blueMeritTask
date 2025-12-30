a78110887fd1b36c20ee6bfe037d8e96f5c12d43
Maaj <maajb1122@gmail.com>
2025-12-30 12:23:53 +0530

    correcting the app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,41 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import ProtectedRoute from "./auth/ProtectedRoute.jsx";
 import Navbar from "./components/Navbar";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import AdminDashboard from "./pages/AdminDashboard";
 import NotFound from "./pages/NotFound";
+import { useAuth } from "./auth/AuthContext.jsx";
 
 const App = () => {
+  const { user } = useAuth();
+
   return (
     <BrowserRouter>
       <Navbar />
 
       <Routes>
-        
+        <Route
+          path="/"
+          element={
+            user ? (
+              user.role === "admin" ? (
+                <Navigate to="/admin" replace />
+              ) : (
+                <Navigate to="/profile" replace />
+              )
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        
         <Route
           path="/profile"
           element={
@@ -28,7 +45,7 @@ const App = () => {
           }
         />
 
-        
+       
         <Route
           path="/admin"
           element={
@@ -38,7 +55,6 @@ const App = () => {
           }
         />
 
-        
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
